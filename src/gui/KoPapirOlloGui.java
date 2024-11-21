1d2b51150f6e1bed1f7ae81ba775ae31614d70f2
KisváradiMárton(Szf_ <kisvaradi.marci@gmail.com>
2024-11-21 16:25:44 +0100

    gui radio gomb, start, bezar gomb mukodik, txt areaba infokat irja

--- src/gui/KoPapirOlloGui.java
+++ src/gui/KoPapirOlloGui.java
@@ -4,12 +4,17 @@
  */
 package gui;
 
-/**
- *
- * @author KisváradiMárton(Szf_
- */
+import java.util.Random;
+
 public class KoPapirOlloGui extends javax.swing.JFrame {
 
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new KoPapirOlloGui().setVisible(true);
+            }
+        });
+    }
     /**
      * Creates new form KoPapirOlloGui
      */
@@ -31,7 +36,6 @@ public class KoPapirOlloGui extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         txtAreaResult = new javax.swing.JTextArea();
         jLabel1 = new javax.swing.JLabel();
-        btnRestart = new javax.swing.JButton();
         btnClose = new javax.swing.JButton();
         radioKo = new javax.swing.JRadioButton();
         jLabel2 = new javax.swing.JLabel();
@@ -43,20 +47,19 @@ public class KoPapirOlloGui extends javax.swing.JFrame {
 
         jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder("Kő-Papír-Olló játék"));
 
+        txtAreaResult.setEditable(false);
         txtAreaResult.setColumns(20);
         txtAreaResult.setRows(5);
         jScrollPane1.setViewportView(txtAreaResult);
 
         jLabel1.setText("Eredmény:");
 
-        btnRestart.setText("Újra");
-        btnRestart.addActionListener(new java.awt.event.ActionListener() {
+        btnClose.setText("Bezárás");
+        btnClose.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnRestartActionPerformed(evt);
+                btnCloseActionPerformed(evt);
             }
         });
-
-        btnClose.setText("Bezárás");
 
         buttonGroup1.add(radioKo);
         radioKo.setText("Kő");
@@ -96,35 +99,30 @@ public class KoPapirOlloGui extends javax.swing.JFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(66, 66, 66)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(66, 66, 66)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1)
                             .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addGap(32, 32, 32)
-                                .addComponent(radioKo)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(radioKo)
+                                    .addComponent(jLabel1))
                                 .addGap(18, 18, 18)
                                 .addComponent(radioPapir)
                                 .addGap(18, 18, 18)
-                                .addComponent(radioOllo)))
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(11, 11, 11)
-                        .addComponent(btnRestart)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 69, Short.MAX_VALUE)
-                        .addComponent(btnClose)
-                        .addGap(76, 76, 76))))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addComponent(radioOllo))))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(131, 131, 131)
                         .addComponent(jLabel2))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(139, 139, 139)
+                        .addGap(145, 145, 145)
                         .addComponent(btnStart)))
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addGap(0, 66, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnClose)
+                .addGap(135, 135, 135))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -142,11 +140,9 @@ public class KoPapirOlloGui extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnRestart)
-                    .addComponent(btnClose))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnClose)
+                .addContainerGap(12, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -169,12 +165,8 @@ public class KoPapirOlloGui extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnRestartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRestartActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_btnRestartActionPerformed
-
     private void radioKoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_radioKoActionPerformed
-       
+
     }//GEN-LAST:event_radioKoActionPerformed
 
     private void radioPapirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_radioPapirActionPerformed
@@ -186,47 +178,55 @@ public class KoPapirOlloGui extends javax.swing.JFrame {
     }//GEN-LAST:event_radioOlloActionPerformed
 
     private void btnStartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnStartActionPerformed
-        // TODO add your handling code here:
+        Random random = new Random();
+        boolean koValasztas = radioKo.isSelected();
+        boolean papirValasztas = radioPapir.isSelected();
+        boolean olloValasztas = radioOllo.isSelected();
+        
+        String[] valaszthatok = {"kő", "papír", "olló"};
+        
+        int felhasznSzam = 0;
+        if (koValasztas) {
+            felhasznSzam = 1;
+        } else if (papirValasztas) {
+            felhasznSzam = 2;
+        } else if (olloValasztas) {
+            felhasznSzam = 3;
+        } else {
+            txtAreaResult.setText("Kérlek válassz ki valamit!");
+        }
+        String felhasznValasztas = valaszthatok[felhasznSzam - 1];
+        int gepSzam = random.nextInt(3) + 1;
+        String gepValasztas = valaszthatok[gepSzam - 1];
+        
+        txtAreaResult.setText("A választásod: " + felhasznValasztas + "\n"
+                              + "A gép válaszása: " + gepValasztas);
+        String meglevoSzoveg = txtAreaResult.getText();
+        
+        if (felhasznSzam == gepSzam) {
+            txtAreaResult.setText(meglevoSzoveg + "\n" + "Döntetlen!");
+        } else if (felhasznSzam == 1 && gepSzam == 3 || 
+                   felhasznSzam == 2 && gepSzam == 1 ||
+                   felhasznSzam == 3 && gepSzam == 2) {
+            txtAreaResult.setText(meglevoSzoveg + "\n" + "Nyertél!");
+        } else if (felhasznSzam == 1 && gepSzam == 2 ||
+                   felhasznSzam == 2 && gepSzam == 3 ||
+                   felhasznSzam == 3 && gepSzam == 1) {
+            txtAreaResult.setText(meglevoSzoveg + "\n" + "Vesztettél!");
+        }
     }//GEN-LAST:event_btnStartActionPerformed
+
+    private void btnCloseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCloseActionPerformed
+        final int HIBA_KOD = 0;
+        System.exit(HIBA_KOD);
+    }//GEN-LAST:event_btnCloseActionPerformed
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(KoPapirOlloGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(KoPapirOlloGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(KoPapirOlloGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(KoPapirOlloGui.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new KoPapirOlloGui().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnClose;
-    private javax.swing.JButton btnRestart;
     private javax.swing.JButton btnStart;
     private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JLabel jLabel1;
